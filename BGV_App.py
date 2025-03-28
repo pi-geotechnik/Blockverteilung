0e2bf169d2462338ba70bef8b43ed515037c0d83
pi-geotechnik <mariella.illeditsch@pi-geo.at>
2025-03-28 11:14:53 +0100

    Add files via upload

--- BGV_App.py
+++ BGV_App.py
@@ -232,7 +232,7 @@ if einheit == "Volumen in m³":
             
             # Zeige die erfolgreiche Meldung an
             st.success("Die Beispiel-Datei 'Dachsteinkalk' wurde erfolgreich geladen.")
-            st.warning("Achtung: eine bereits zuvor hochgeladene 'Eigene Liste mit m³-Werten' muss unten (Klick auf 'X') entfernt und die Beispiel-Datei neu geladen werden!")
+            st.warning("Achtung: eine bereits zuvor hochgeladene 'Eigene Liste mit m³-Werten' muss entfernt (Klick auf 'x') und die Beispiel-Datei neu geladen werden!")
             
             # Verarbeite die Datei, als ob sie über den file_uploader hochgeladen wurde
             file_content = uploaded_file.read().decode("utf-8")  # Beispiel: als Textdatei lesen
